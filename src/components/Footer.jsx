343b7aac5f16cf68df93ce7177dfff4d04c7ab29
joshuabenedict-665 <joshuabenedict665@gmail.com>
2025-09-28 14:35:59 +0530

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="p-6 text-center bg-gray-100 text-gray-600">
+      <p>© 2025 Real-Time Competitor Strategy Tracker. All Rights Reserved.</p>
+    </footer>
+  );
+}
